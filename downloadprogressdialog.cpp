7491e3f552f1f4000ffc4478d980a9e27737124c
thenaserov <naser.rezayi.nr@gmail.com>
2025-08-01 08:23:06 +0330

    [REF]

--- downloadprogressdialog.cpp
+++ downloadprogressdialog.cpp
@@ -1,3 +1,29 @@
 #include "downloadprogressdialog.h"
 
+DownloadProgressDialog::DownloadProgressDialog(QWidget *parent)
+    : QDialog(parent), progressBar(new QProgressBar), label(new QLabel("Downloading...")) {
+    setWindowTitle("Download Progress");
+    progressBar->setRange(0, 100);
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(label);
+    layout->addWidget(progressBar);
+    setLayout(layout);
+    resize(300, 100);
+}
 
+void DownloadProgressDialog::updateProgress(qint64 received, qint64 total)
+{
+    if (total > 0) {
+        int percent = static_cast<int>((100.0 * received) / total);
+        progressBar->setValue(percent);
+        label->setText(QString("Downloaded %1 of %2")
+                           .arg(QLocale().formattedDataSize(received))
+                           .arg(QLocale().formattedDataSize(total)));
+    }
+}
+
+void DownloadProgressDialog::downloadFinished()
+{
+    label->setText("Download complete!");
+    progressBar->setValue(100);
+}
--- downloadprogressdialog.h
+++ downloadprogressdialog.h
@@ -10,32 +10,12 @@
 class DownloadProgressDialog : public QDialog {
     Q_OBJECT
 public:
-    DownloadProgressDialog(QWidget *parent = nullptr)
-        : QDialog(parent), progressBar(new QProgressBar), label(new QLabel("Downloading...")) {
-        setWindowTitle("Download Progress");
-        progressBar->setRange(0, 100);
-        QVBoxLayout *layout = new QVBoxLayout;
-        layout->addWidget(label);
-        layout->addWidget(progressBar);
-        setLayout(layout);
-        resize(300, 100);
-    }
+    DownloadProgressDialog(QWidget *parent = nullptr);
 
 public slots:
-    void updateProgress(qint64 received, qint64 total) {
-        if (total > 0) {
-            int percent = static_cast<int>((100.0 * received) / total);
-            progressBar->setValue(percent);
-            label->setText(QString("Downloaded %1 of %2")
-                               .arg(QLocale().formattedDataSize(received))
-                               .arg(QLocale().formattedDataSize(total)));
-        }
-    }
+    void updateProgress(qint64 received, qint64 total);
 
-    void downloadFinished() {
-        label->setText("Download complete!");
-        progressBar->setValue(100);
-    }
+    void downloadFinished();
 
 private:
     QProgressBar *progressBar;
